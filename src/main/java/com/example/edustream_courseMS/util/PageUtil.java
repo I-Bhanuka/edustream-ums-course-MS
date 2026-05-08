020ea3a1ca95029d26aa773ad946662eaab40c07
Isith Bhanuka <isithbhanuka2006@gmail.com>
2026-05-08 16:04:46 +0530

    feat: introduce page response dto to handle pagination

---
+++ src/main/java/com/example/edustream_courseMS/util/PageUtil.java
@@ -0,0 +1,20 @@
+package com.example.edustream_courseMS.util;
+
+import com.example.edustream_courseMS.dto.responseDTO.PageResponseDTO;
+import org.springframework.data.domain.Page;
+
+public class PageUtil {
+
+    private PageUtil() {} // prevent instantiation
+
+    public static <T> PageResponseDTO<T> toPageResponse(Page<T> page) {
+        return PageResponseDTO.<T>builder()
+                .content(page.getContent())
+                .pageNumber(page.getNumber())
+                .pageSize(page.getSize())
+                .totalElements(page.getTotalElements())
+                .totalPages(page.getTotalPages())
+                .last(page.isLast())
+                .build();
+    }
+}
