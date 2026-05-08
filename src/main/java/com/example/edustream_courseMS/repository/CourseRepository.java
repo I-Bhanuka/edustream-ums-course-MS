a697b1c55122c54db18ca939cd89944e56f909c5
Isith Bhanuka <isithbhanuka2006@gmail.com>
2026-05-08 16:25:01 +0530

    feat: add course retrieval by id repo method

--- src/main/java/com/example/edustream_courseMS/repository/CourseRepository.java
+++ src/main/java/com/example/edustream_courseMS/repository/CourseRepository.java
@@ -3,7 +3,16 @@ package com.example.edustream_courseMS.repository;
 import com.example.edustream_courseMS.entity.Course;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
 import java.util.UUID;
 
 public interface CourseRepository extends JpaRepository<Course, UUID> {
+
+    /**
+     * Query,
+     * SELECT *
+     * FROM course
+     * WHERE courseId = ?;
+     */
+    Optional<Course> findByCourseId(String courseId);
 }
