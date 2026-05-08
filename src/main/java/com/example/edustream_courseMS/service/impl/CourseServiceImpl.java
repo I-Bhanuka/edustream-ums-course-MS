b580e8937238a05b3942b108f43a9b9d4bf3d700
Isith Bhanuka <isithbhanuka2006@gmail.com>
2026-05-08 15:50:14 +0530

    feat: implement the course registration logic

--- src/main/java/com/example/edustream_courseMS/service/impl/CourseServiceImpl.java
+++ src/main/java/com/example/edustream_courseMS/service/impl/CourseServiceImpl.java
@@ -1,5 +1,10 @@
 package com.example.edustream_courseMS.service.impl;
 
+import com.example.edustream_courseMS.dto.requestDTO.RegisterCourseRequestDTO;
+import com.example.edustream_courseMS.dto.responseDTO.RegisterCourseResponseDTO;
+import com.example.edustream_courseMS.entity.Course;
+import com.example.edustream_courseMS.enums.CourseStatus;
+import com.example.edustream_courseMS.repository.CourseRepository;
 import com.example.edustream_courseMS.service.CourseService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -10,9 +15,44 @@ import org.springframework.stereotype.Service;
 @RequiredArgsConstructor
 public class CourseServiceImpl implements CourseService {
 
+    private final CourseRepository courseRepository;
+
     @Override
     public String testService() {
         log.info("CourseServiceImpl: testEndpoint called");
         return "Hello from Course Microservice Service Layer!";
+    }
+
+    @Override
+    public RegisterCourseResponseDTO registerCourse(RegisterCourseRequestDTO registerCourseRequestDTO) {
+
+        log.info("================================ Registering New Course ==============================");
+
+        log.info("Register Request details - Course Name: {}, Course Code: {}, Badge: {}, Duration(days): {}",
+                 registerCourseRequestDTO.getCourseName(),
+                 registerCourseRequestDTO.getCourseId(),
+                 registerCourseRequestDTO.getBadge(),
+                 registerCourseRequestDTO.getDurationDays());
+
+        // Create a new Course entity with the requested data
+        Course registerCourse = Course.builder()
+                .courseId(registerCourseRequestDTO.getCourseId())
+                .courseName(registerCourseRequestDTO.getCourseName())
+                .badge(registerCourseRequestDTO.getBadge())
+                .durationDays(registerCourseRequestDTO.getDurationDays())
+                .courseStatus(CourseStatus.SCHEDULED)
+                .build();
+
+        // Save the new Course to the database
+        log.info("Saving new Course to database with course ID: {}", registerCourse.getCourseId());
+        courseRepository.save(registerCourse);
+
+        return RegisterCourseResponseDTO.builder()
+                .courseId(registerCourse.getCourseId())
+                .courseName(registerCourse.getCourseName())
+                .badge(registerCourse.getBadge())
+                .durationDays(registerCourse.getDurationDays())
+                .courseStatus(registerCourse.getCourseStatus())
+                .build();
     }
 }
