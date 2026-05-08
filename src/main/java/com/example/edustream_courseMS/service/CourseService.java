add6d4ac0cb8773d0a284297d5d5088da290e3db
Isith Bhanuka <isithbhanuka2006@gmail.com>
2026-05-08 15:49:53 +0530

    feat: add Course registration abstract method

--- src/main/java/com/example/edustream_courseMS/service/CourseService.java
+++ src/main/java/com/example/edustream_courseMS/service/CourseService.java
@@ -1,5 +1,10 @@
 package com.example.edustream_courseMS.service;
 
+import com.example.edustream_courseMS.dto.requestDTO.RegisterCourseRequestDTO;
+import com.example.edustream_courseMS.dto.responseDTO.RegisterCourseResponseDTO;
+
 public interface CourseService {
     String testService();
+
+    RegisterCourseResponseDTO registerCourse(RegisterCourseRequestDTO registerCourseRequestDTO);
 }
