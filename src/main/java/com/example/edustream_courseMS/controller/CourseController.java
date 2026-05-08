0aab32ccc6040f676ad6d2d2185d74760aab25df
Isith Bhanuka <isithbhanuka2006@gmail.com>
2026-05-08 16:12:11 +0530

    feat: add the endpoint for the all courses retrieval

--- src/main/java/com/example/edustream_courseMS/controller/CourseController.java
+++ src/main/java/com/example/edustream_courseMS/controller/CourseController.java
@@ -2,10 +2,15 @@ package com.example.edustream_courseMS.controller;
 
 import com.example.edustream_courseMS.dto.responseDTO.ApiResponse;
 import com.example.edustream_courseMS.dto.requestDTO.RegisterCourseRequestDTO;
+import com.example.edustream_courseMS.dto.responseDTO.PageResponseDTO;
 import com.example.edustream_courseMS.dto.responseDTO.RegisterCourseResponseDTO;
+import com.example.edustream_courseMS.entity.Course;
 import com.example.edustream_courseMS.service.CourseService;
+import com.example.edustream_courseMS.util.PageUtil;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -24,7 +29,7 @@ public class CourseController {
     }
 
     @PostMapping("/create")
-    public ResponseEntity<ApiResponse<RegisterCourseResponseDTO>> registerStudent(
+    public ResponseEntity<ApiResponse<RegisterCourseResponseDTO>> registerCourse(
             @Valid @RequestBody RegisterCourseRequestDTO registerCourseRequestDTO) {
 
         RegisterCourseResponseDTO response = courseService.registerCourse(registerCourseRequestDTO);
@@ -35,6 +40,20 @@ public class CourseController {
                         .success(true)
                         .message("Course registered successfully")
                         .data(response)
+                        .build());
+    }
+
+    @PostMapping("/all")
+    public ResponseEntity<ApiResponse<PageResponseDTO<Course>>> getAllCourses(Pageable pageable) {
+
+        Page<Course> response = courseService.getAllCourses(pageable);
+
+        return ResponseEntity
+                .status(HttpStatus.OK)
+                .body(ApiResponse.<PageResponseDTO<Course>>builder()
+                        .success(true)
+                        .message("Courses retrieved successfully")
+                        .data(PageUtil.toPageResponse(response))
                         .build());
     }
 }
