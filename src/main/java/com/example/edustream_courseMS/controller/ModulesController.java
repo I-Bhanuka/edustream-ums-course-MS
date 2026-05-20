35aad0ea84d49a43b8c372041566c6eb5e9a38c8
Isith Bhanuka <isithbhanuka2006@gmail.com>
2026-05-20 14:27:56 +0530

    feat: add get by module code controller endpoint

--- src/main/java/com/example/edustream_courseMS/controller/ModulesController.java
+++ src/main/java/com/example/edustream_courseMS/controller/ModulesController.java
@@ -1,7 +1,9 @@
 package com.example.edustream_courseMS.controller;
 
+import com.example.edustream_courseMS.dto.requestDTO.ModuleRequestByModuleCode;
 import com.example.edustream_courseMS.dto.requestDTO.RegisterModuleRequestDTO;
 import com.example.edustream_courseMS.dto.responseDTO.ApiResponse;
+import com.example.edustream_courseMS.dto.responseDTO.ModuleRequestResponseDTO;
 import com.example.edustream_courseMS.dto.responseDTO.RegisterModuleResponseDTO;
 import com.example.edustream_courseMS.service.ModulesService;
 import jakarta.validation.Valid;
@@ -19,9 +21,10 @@ public class ModulesController {
     private final ModulesService modulesService;
 
     @PostMapping("/create")
-    public ResponseEntity<ApiResponse<RegisterModuleResponseDTO>> createModule(@Valid @RequestBody RegisterModuleRequestDTO registerModuleRequestDTO) {
+    public ResponseEntity<ApiResponse<RegisterModuleResponseDTO>> createModule(
+            @Valid @RequestBody RegisterModuleRequestDTO registerModuleRequestDTO) {
 
-        RegisterModuleResponseDTO responseDTO = modulesService.registerModule(registerModuleRequestDTO);
+        RegisterModuleResponseDTO responseDTO = modulesService.registerModuleService(registerModuleRequestDTO);
 
         return ResponseEntity.status(HttpStatus.CREATED)
                 .body(ApiResponse.<RegisterModuleResponseDTO>builder()
@@ -29,6 +32,21 @@ public class ModulesController {
                         .message("Module created successfully")
                         .data(responseDTO)
                         .build());
+    }
+
+
+    @PostMapping("/getModuleByModuleCode")
+    public ResponseEntity<ApiResponse<ModuleRequestResponseDTO>> getModuleByModuleCode(
+            @Valid @RequestBody ModuleRequestByModuleCode moduleRequestByModuleCode) {
+
+            ModuleRequestResponseDTO responseDTO = modulesService.getModuleByModuleCodeService(moduleRequestByModuleCode);
+
+            return ResponseEntity.status(HttpStatus.OK)
+                    .body(ApiResponse.<ModuleRequestResponseDTO>builder()
+                            .success(true)
+                            .message("Module retrieved successfully")
+                            .data(responseDTO)
+                            .build());
     }
 
 
