454a90f1e7eb5e3862b684a87ad38941c89e0901
Isith Bhanuka <isithbhanuka2006@gmail.com>
2026-05-20 14:44:34 +0530

    feat: add findAll query method without the UUID

--- src/main/java/com/example/edustream_courseMS/repository/ModulesRepository.java
+++ src/main/java/com/example/edustream_courseMS/repository/ModulesRepository.java
@@ -1,7 +1,11 @@
 package com.example.edustream_courseMS.repository;
 
+import com.example.edustream_courseMS.dto.responseDTO.ModuleRequestResponseDTO;
 import com.example.edustream_courseMS.entity.Modules;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
@@ -11,4 +15,14 @@ import java.util.UUID;
 public interface ModulesRepository extends JpaRepository<Modules, UUID> {
 
     Optional<Modules> findByModuleCode(String moduleCode);
+
+    @Query("""
+        SELECT new com.example.edustream_courseMS.dto.responseDTO.ModuleRequestResponseDTO(
+            m.moduleName,
+            m.moduleCode,
+            m.credit
+        )
+        FROM Modules m
+    """)
+    Page<ModuleRequestResponseDTO> findAllWithoutUUID(Pageable pageable);
 }
