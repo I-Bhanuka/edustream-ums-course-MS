cb5b1f647519942ee25a4d7170083ba849cc5046
Isith Bhanuka <isithbhanuka2006@gmail.com>
2026-05-20 14:27:10 +0530

    feat: add get by module code service method contract

--- src/main/java/com/example/edustream_courseMS/service/ModulesService.java
+++ src/main/java/com/example/edustream_courseMS/service/ModulesService.java
@@ -1,9 +1,13 @@
 package com.example.edustream_courseMS.service;
 
+import com.example.edustream_courseMS.dto.requestDTO.ModuleRequestByModuleCode;
 import com.example.edustream_courseMS.dto.requestDTO.RegisterModuleRequestDTO;
+import com.example.edustream_courseMS.dto.responseDTO.ModuleRequestResponseDTO;
 import com.example.edustream_courseMS.dto.responseDTO.RegisterModuleResponseDTO;
 
 public interface ModulesService {
 
-    RegisterModuleResponseDTO registerModule(RegisterModuleRequestDTO registerModuleRequestDTO);
+    RegisterModuleResponseDTO registerModuleService(RegisterModuleRequestDTO registerModuleRequestDTO);
+
+    ModuleRequestResponseDTO getModuleByModuleCodeService(ModuleRequestByModuleCode moduleRequestByModuleCode);
 }
