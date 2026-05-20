b8264b60063ce3029b6da971e0aff83ec4c91bfe
Isith Bhanuka <isithbhanuka2006@gmail.com>
2026-05-20 14:08:19 +0530

    feat: add Modules Repository interface for DB communication

---
+++ src/main/java/com/example/edustream_courseMS/repository/ModulesRepository.java
@@ -0,0 +1,11 @@
+package com.example.edustream_courseMS.repository;
+
+import com.example.edustream_courseMS.entity.Modules;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.UUID;
+
+@Repository
+public interface ModulesRepository extends JpaRepository<Modules, UUID> {
+}
