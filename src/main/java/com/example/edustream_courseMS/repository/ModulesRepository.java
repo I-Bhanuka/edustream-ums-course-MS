6f1058444d2a4d4580149789ece28ef8ed4cd35e
Isith Bhanuka <isithbhanuka2006@gmail.com>
2026-05-20 14:25:28 +0530

    feat: add query method to query module by code

--- src/main/java/com/example/edustream_courseMS/repository/ModulesRepository.java
+++ src/main/java/com/example/edustream_courseMS/repository/ModulesRepository.java
@@ -4,8 +4,11 @@ import com.example.edustream_courseMS.entity.Modules;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
 import java.util.UUID;
 
 @Repository
 public interface ModulesRepository extends JpaRepository<Modules, UUID> {
+
+    Optional<Modules> findByModuleCode(String moduleCode);
 }
