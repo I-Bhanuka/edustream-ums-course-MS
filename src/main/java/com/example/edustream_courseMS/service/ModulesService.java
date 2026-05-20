c672d76184e88ae38b7b5c819e9d74ab6dddf672
Isith Bhanuka <isithbhanuka2006@gmail.com>
2026-05-20 14:45:00 +0530

    feat: add getAll modules contract method

--- src/main/java/com/example/edustream_courseMS/service/ModulesService.java
+++ src/main/java/com/example/edustream_courseMS/service/ModulesService.java
@@ -4,10 +4,14 @@ import com.example.edustream_courseMS.dto.requestDTO.ModuleRequestByModuleCode;
 import com.example.edustream_courseMS.dto.requestDTO.RegisterModuleRequestDTO;
 import com.example.edustream_courseMS.dto.responseDTO.ModuleRequestResponseDTO;
 import com.example.edustream_courseMS.dto.responseDTO.RegisterModuleResponseDTO;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 
 public interface ModulesService {
 
     RegisterModuleResponseDTO registerModuleService(RegisterModuleRequestDTO registerModuleRequestDTO);
 
     ModuleRequestResponseDTO getModuleByModuleCodeService(ModuleRequestByModuleCode moduleRequestByModuleCode);
+
+    Page<ModuleRequestResponseDTO> getAllModulesService(Pageable pageable);
 }
