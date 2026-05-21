6f6c4e8605eebf938400984da986ba9a93281072
Isith Bhanuka <isithbhanuka2006@gmail.com>
2026-05-21 09:45:55 +0530

    feat: add getAllSemestersService contract method into the interface

--- src/main/java/com/example/edustream_courseMS/service/SemesterService.java
+++ src/main/java/com/example/edustream_courseMS/service/SemesterService.java
@@ -4,10 +4,15 @@ import com.example.edustream_courseMS.dto.requestDTO.RegisterSemesterRequestDTO;
 import com.example.edustream_courseMS.dto.requestDTO.RequestSemesterById;
 import com.example.edustream_courseMS.dto.responseDTO.RegisterSemesterResponseDTO;
 import com.example.edustream_courseMS.dto.responseDTO.SemesterRequestResponseDTO;
+import com.example.edustream_courseMS.entity.Semester;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 
 public interface SemesterService {
 
     RegisterSemesterResponseDTO registerSemesterService(RegisterSemesterRequestDTO registerSemesterRequestDTO);
 
     SemesterRequestResponseDTO getSemesterByIdService(RequestSemesterById requestSemesterById);
+
+    Page<Semester> getAllSemestersService(Pageable pageable);
 }
