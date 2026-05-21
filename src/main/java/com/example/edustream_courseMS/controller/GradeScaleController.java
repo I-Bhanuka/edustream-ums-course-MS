9045f8617e6cd6533a0fbe291d28a855920abcc0
Isith Bhanuka <isithbhanuka2006@gmail.com>
2026-05-21 11:06:43 +0530

    feat: add controller endpoint for the registerGradeScaleService

---
+++ src/main/java/com/example/edustream_courseMS/controller/GradeScaleController.java
@@ -0,0 +1,37 @@
+package com.example.edustream_courseMS.controller;
+
+import com.example.edustream_courseMS.dto.requestDTO.RegisterCourseRequestDTO;
+import com.example.edustream_courseMS.dto.requestDTO.RegisterGradeScaleRequestDTO;
+import com.example.edustream_courseMS.dto.responseDTO.ApiResponse;
+import com.example.edustream_courseMS.dto.responseDTO.RegisterGradeScaleResponseDTO;
+import com.example.edustream_courseMS.entity.GradeScale;
+import com.example.edustream_courseMS.service.GradeScaleService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/grade-scale")
+@RequiredArgsConstructor
+public class GradeScaleController {
+
+    private final GradeScaleService gradeScaleService;
+
+    @PostMapping("/registerScale")
+    public ResponseEntity<ApiResponse<RegisterGradeScaleResponseDTO>> registerScale(
+            @RequestBody RegisterGradeScaleRequestDTO registerGradeScaleRequestDTO) {
+
+        RegisterGradeScaleResponseDTO responseDTO = gradeScaleService.registerGradeScaleService(registerGradeScaleRequestDTO);
+
+        return ResponseEntity.status(HttpStatus.CREATED).
+                body(ApiResponse.<RegisterGradeScaleResponseDTO>builder()
+                        .success(true)
+                        .message("Grade scale registered successfully")
+                        .data(responseDTO)
+                        .build());
+    }
+}
