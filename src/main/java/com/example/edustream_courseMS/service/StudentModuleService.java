9a474543d06e50348c4d7d5571f7951d6b546e2e
Isith Bhanuka <isithbhanuka2006@gmail.com>
2026-05-21 08:36:21 +0530

    feat: add getAllEnrollmentsService contract service method into the interface

--- src/main/java/com/example/edustream_courseMS/service/StudentModuleService.java
+++ src/main/java/com/example/edustream_courseMS/service/StudentModuleService.java
@@ -3,10 +3,15 @@ package com.example.edustream_courseMS.service;
 import com.example.edustream_courseMS.dto.requestDTO.EnrollStudentToModuleRequestDTO;
 import com.example.edustream_courseMS.dto.requestDTO.RequestModuleEnrollmentById;
 import com.example.edustream_courseMS.dto.responseDTO.EnrollStudentToModuleResponseDTO;
+import com.example.edustream_courseMS.entity.StudentModule;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 
 public interface StudentModuleService {
 
     EnrollStudentToModuleResponseDTO enrollStudentInModuleService(EnrollStudentToModuleRequestDTO enrollStudentToModuleRequestDTO);
 
     EnrollStudentToModuleResponseDTO getEnrollmentByIdService(RequestModuleEnrollmentById requestModuleEnrollmentById);
+
+    Page<StudentModule> getAllEnrollmentsService(Pageable pageable);
 }
