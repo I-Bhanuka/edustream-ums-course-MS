8f92cdeee8151aaca1d3655b84d299ed7fbb259a
Isith Bhanuka <isithbhanuka2006@gmail.com>
2026-05-21 12:55:33 +0530

    feat: add getAllResultsService contract method into the interface

--- src/main/java/com/example/edustream_courseMS/service/ResultsService.java
+++ src/main/java/com/example/edustream_courseMS/service/ResultsService.java
@@ -5,11 +5,16 @@ import com.example.edustream_courseMS.dto.requestDTO.PostResultsRequestDTO;
 import com.example.edustream_courseMS.dto.requestDTO.ResultsRequestByEnrollmentIdDTO;
 import com.example.edustream_courseMS.dto.responseDTO.PostResultsResponseDTO;
 import com.example.edustream_courseMS.dto.responseDTO.ResultsByEnrollmentResponseDTO;
+import com.example.edustream_courseMS.dto.responseDTO.ResultsResponseDTO;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 
 public interface ResultsService {
 
     PostResultsResponseDTO postResultsService(PostResultsRequestDTO postResultsRequestDTO);
 
     ResultsByEnrollmentResponseDTO getResultsByEnrollmentIdService(ResultsRequestByEnrollmentIdDTO resultsRequestByEnrollmentIdDTO);
+
+    Page<ResultsResponseDTO> getAllResultsService(Pageable pageable);
 
 }
