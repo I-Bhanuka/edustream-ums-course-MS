e1cf038ef202b4d5de0a40988e01c22a2d3a6ca8
Isith Bhanuka <isithbhanuka2006@gmail.com>
2026-05-21 12:33:10 +0530

    feat: add findByEnrollmentId query method

--- src/main/java/com/example/edustream_courseMS/repository/ResultsRepository.java
+++ src/main/java/com/example/edustream_courseMS/repository/ResultsRepository.java
@@ -3,7 +3,10 @@ package com.example.edustream_courseMS.repository;
 import com.example.edustream_courseMS.entity.Results;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
 import java.util.UUID;
 
 public interface ResultsRepository extends JpaRepository<Results, UUID> {
+
+    Optional<Results> findByEnrollmentId(UUID enrollmentId);
 }
