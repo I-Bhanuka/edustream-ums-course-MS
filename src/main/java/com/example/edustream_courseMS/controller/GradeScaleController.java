200b53794cd0751bffec2840a497e0a891a06b49
Isith Bhanuka <isithbhanuka2006@gmail.com>
2026-05-21 11:20:06 +0530

    feat: add controller endpoint for the getGradeScaleByGradeService

--- src/main/java/com/example/edustream_courseMS/controller/GradeScaleController.java
+++ src/main/java/com/example/edustream_courseMS/controller/GradeScaleController.java
@@ -2,10 +2,13 @@ package com.example.edustream_courseMS.controller;
 
 import com.example.edustream_courseMS.dto.requestDTO.RegisterCourseRequestDTO;
 import com.example.edustream_courseMS.dto.requestDTO.RegisterGradeScaleRequestDTO;
+import com.example.edustream_courseMS.dto.requestDTO.RequestGradeScaleByGrade;
 import com.example.edustream_courseMS.dto.responseDTO.ApiResponse;
+import com.example.edustream_courseMS.dto.responseDTO.GradeScaleRequestResponseDTO;
 import com.example.edustream_courseMS.dto.responseDTO.RegisterGradeScaleResponseDTO;
 import com.example.edustream_courseMS.entity.GradeScale;
 import com.example.edustream_courseMS.service.GradeScaleService;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -31,6 +34,21 @@ public class GradeScaleController {
                 body(ApiResponse.<RegisterGradeScaleResponseDTO>builder()
                         .success(true)
                         .message("Grade scale registered successfully")
+                        .data(responseDTO)
+                        .build());
+    }
+
+
+    @PostMapping("/getByGrade")
+    public ResponseEntity<ApiResponse<GradeScaleRequestResponseDTO>> getGradeScaleByGrade(
+            @Valid @RequestBody RequestGradeScaleByGrade requestGradeScaleByGrade) {
+
+        GradeScaleRequestResponseDTO responseDTO = gradeScaleService.getGradeScaleByGradeService(requestGradeScaleByGrade);
+
+        return ResponseEntity.status(HttpStatus.OK)
+                .body(ApiResponse.<GradeScaleRequestResponseDTO>builder()
+                        .success(true)
+                        .message("Grade scale retrieved successfully gy grade: " + requestGradeScaleByGrade.getGrade())
                         .data(responseDTO)
                         .build());
     }
