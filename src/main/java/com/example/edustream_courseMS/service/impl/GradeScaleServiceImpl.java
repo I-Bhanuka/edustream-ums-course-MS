b3b29e1d6f40c5a2851394bda8f9c511b61d9ec2
Isith Bhanuka <isithbhanuka2006@gmail.com>
2026-05-21 11:19:50 +0530

    feat: add implementation for the getGradeScaleByGradeService

--- src/main/java/com/example/edustream_courseMS/service/impl/GradeScaleServiceImpl.java
+++ src/main/java/com/example/edustream_courseMS/service/impl/GradeScaleServiceImpl.java
@@ -2,11 +2,14 @@ package com.example.edustream_courseMS.service.impl;
 
 import com.example.edustream_courseMS.dto.requestDTO.RegisterCourseRequestDTO;
 import com.example.edustream_courseMS.dto.requestDTO.RegisterGradeScaleRequestDTO;
+import com.example.edustream_courseMS.dto.requestDTO.RequestGradeScaleByGrade;
+import com.example.edustream_courseMS.dto.responseDTO.GradeScaleRequestResponseDTO;
 import com.example.edustream_courseMS.dto.responseDTO.RegisterCourseResponseDTO;
 import com.example.edustream_courseMS.dto.responseDTO.RegisterGradeScaleResponseDTO;
 import com.example.edustream_courseMS.entity.Course;
 import com.example.edustream_courseMS.entity.GradeScale;
 import com.example.edustream_courseMS.enums.CourseStatus;
+import com.example.edustream_courseMS.exception.GradeScaleNotFoundException;
 import com.example.edustream_courseMS.repository.GradeScaleRepository;
 import com.example.edustream_courseMS.service.GradeScaleService;
 import lombok.RequiredArgsConstructor;
@@ -51,4 +54,29 @@ public class GradeScaleServiceImpl implements GradeScaleService {
                 .build();
 
     }
+
+    public GradeScaleRequestResponseDTO getGradeScaleByGradeService(RequestGradeScaleByGrade requestGradeScaleByGrade) {
+
+        log.info("================================ Retrieving Grade Scale by Grade ==============================");
+
+        log.info("Request details - Grade: {}", requestGradeScaleByGrade.getGrade());
+
+        // Retrieve the grade scale from the database based on the provided grade
+        GradeScale gradeScale = gradeScaleRepository.findByGrade(requestGradeScaleByGrade.getGrade())
+                .orElseThrow(() -> new GradeScaleNotFoundException(requestGradeScaleByGrade.getGrade()));
+
+        log.info("Grade Scale retrieved successfully for Grade: {}. Min: {}, Max: {}, GradePoint: {}",
+                requestGradeScaleByGrade.getGrade(),
+                gradeScale.getMin(),
+                gradeScale.getMax(),
+                gradeScale.getGradePoint());
+
+        return GradeScaleRequestResponseDTO.builder()
+                .grade(gradeScale.getGrade())
+                .min(gradeScale.getMin())
+                .max(gradeScale.getMax())
+                .gradePoint(gradeScale.getGradePoint())
+                .build();
+    }
+
 }
