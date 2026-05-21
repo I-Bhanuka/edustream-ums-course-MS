0de96990388ca7bebb9fd31fa76a336c61494572
Isith Bhanuka <isithbhanuka2006@gmail.com>
2026-05-21 11:18:51 +0530

    feat: add getByGrade query method

--- src/main/java/com/example/edustream_courseMS/repository/GradeScaleRepository.java
+++ src/main/java/com/example/edustream_courseMS/repository/GradeScaleRepository.java
@@ -4,8 +4,11 @@ import com.example.edustream_courseMS.entity.GradeScale;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
 import java.util.UUID;
 
 @Repository
 public interface GradeScaleRepository extends JpaRepository<GradeScale, UUID> {
+
+    Optional<GradeScale> findByGrade(String grade);
 }
