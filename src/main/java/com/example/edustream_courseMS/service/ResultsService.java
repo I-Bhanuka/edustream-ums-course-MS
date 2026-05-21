df42fa1de86e35d6a63440722c7e32d134b6802d
Isith Bhanuka <isithbhanuka2006@gmail.com>
2026-05-21 12:33:53 +0530

    feat: add getResultsByEnrollmentIdService contract method into the interface

--- src/main/java/com/example/edustream_courseMS/service/ResultsService.java
+++ src/main/java/com/example/edustream_courseMS/service/ResultsService.java
@@ -2,9 +2,14 @@ package com.example.edustream_courseMS.service;
 
 
 import com.example.edustream_courseMS.dto.requestDTO.PostResultsRequestDTO;
+import com.example.edustream_courseMS.dto.requestDTO.ResultsRequestByEnrollmentIdDTO;
 import com.example.edustream_courseMS.dto.responseDTO.PostResultsResponseDTO;
+import com.example.edustream_courseMS.dto.responseDTO.ResultsByEnrollmentResponseDTO;
 
 public interface ResultsService {
 
     PostResultsResponseDTO postResults(PostResultsRequestDTO postResultsRequestDTO);
+
+    ResultsByEnrollmentResponseDTO getResultsByEnrollmentIdService(ResultsRequestByEnrollmentIdDTO resultsRequestByEnrollmentIdDTO);
+
 }
