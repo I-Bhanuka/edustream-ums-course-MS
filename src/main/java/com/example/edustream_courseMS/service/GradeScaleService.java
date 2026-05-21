90237489ad783c6fdeda927e194943198da11b40
Isith Bhanuka <isithbhanuka2006@gmail.com>
2026-05-21 11:19:29 +0530

    feat: add getGradeScaleByGradeService contract method into the interface

--- src/main/java/com/example/edustream_courseMS/service/GradeScaleService.java
+++ src/main/java/com/example/edustream_courseMS/service/GradeScaleService.java
@@ -2,10 +2,15 @@ package com.example.edustream_courseMS.service;
 
 import com.example.edustream_courseMS.dto.requestDTO.RegisterCourseRequestDTO;
 import com.example.edustream_courseMS.dto.requestDTO.RegisterGradeScaleRequestDTO;
+import com.example.edustream_courseMS.dto.requestDTO.RequestGradeScaleByGrade;
+import com.example.edustream_courseMS.dto.responseDTO.GradeScaleRequestResponseDTO;
 import com.example.edustream_courseMS.dto.responseDTO.RegisterGradeScaleResponseDTO;
 
 public interface GradeScaleService {
 
     RegisterGradeScaleResponseDTO registerGradeScaleService(RegisterGradeScaleRequestDTO registerGradeScaleRequestDTO);
+
+    GradeScaleRequestResponseDTO getGradeScaleByGradeService(RequestGradeScaleByGrade requestGradeScaleByGrade);
+
 
 }
