8f2a2912e76670ec76d8fe0f18fe21dc0b055efc
Isith Bhanuka <isithbhanuka2006@gmail.com>
2026-05-21 09:36:07 +0530

    feat: add getSemesterByIdService contract method into the interface

--- src/main/java/com/example/edustream_courseMS/service/SemesterService.java
+++ src/main/java/com/example/edustream_courseMS/service/SemesterService.java
@@ -1,9 +1,13 @@
 package com.example.edustream_courseMS.service;
 
 import com.example.edustream_courseMS.dto.requestDTO.RegisterSemesterRequestDTO;
+import com.example.edustream_courseMS.dto.requestDTO.RequestSemesterById;
 import com.example.edustream_courseMS.dto.responseDTO.RegisterSemesterResponseDTO;
+import com.example.edustream_courseMS.dto.responseDTO.SemesterRequestResponseDTO;
 
 public interface SemesterService {
 
     RegisterSemesterResponseDTO registerSemesterService(RegisterSemesterRequestDTO registerSemesterRequestDTO);
+
+    SemesterRequestResponseDTO getSemesterByIdService(RequestSemesterById requestSemesterById);
 }
