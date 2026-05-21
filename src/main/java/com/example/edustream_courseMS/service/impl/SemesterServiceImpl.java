a7d169d553735270056457c5366c8ae2b1ccedaf
Isith Bhanuka <isithbhanuka2006@gmail.com>
2026-05-21 09:36:28 +0530

    feat: add getSemesterByIdService implementation

--- src/main/java/com/example/edustream_courseMS/service/impl/SemesterServiceImpl.java
+++ src/main/java/com/example/edustream_courseMS/service/impl/SemesterServiceImpl.java
@@ -1,8 +1,11 @@
 package com.example.edustream_courseMS.service.impl;
 
 import com.example.edustream_courseMS.dto.requestDTO.RegisterSemesterRequestDTO;
+import com.example.edustream_courseMS.dto.requestDTO.RequestSemesterById;
 import com.example.edustream_courseMS.dto.responseDTO.RegisterSemesterResponseDTO;
+import com.example.edustream_courseMS.dto.responseDTO.SemesterRequestResponseDTO;
 import com.example.edustream_courseMS.entity.Semester;
+import com.example.edustream_courseMS.exception.SemesterNotFoundException;
 import com.example.edustream_courseMS.repository.SemesterRepository;
 import com.example.edustream_courseMS.service.SemesterService;
 import lombok.RequiredArgsConstructor;
@@ -52,5 +55,33 @@ public class SemesterServiceImpl implements SemesterService {
                 .endDate(registerModule.getEndDate())
                 .build();
 
+    }
+
+    @Override
+    public SemesterRequestResponseDTO getSemesterByIdService(RequestSemesterById requestSemesterById) {
+
+            log.info("================================ Retrieve Semester by ID ==============================");
+
+            log.info("Retrieve Request details - Semester ID: {}",
+                    requestSemesterById.getSemesterId());
+
+            Semester semester = semesterRepository.findById(requestSemesterById.getSemesterId())
+                    .orElseThrow(() -> new SemesterNotFoundException(requestSemesterById.getSemesterId().toString()));
+
+            log.info("Semester found with ID: {}. Semester Name: {}, Semester Number: {}, Semester Year: {}, Semester Start Date: {}, Semester End Date: {}",
+                    semester.getId(),
+                    semester.getName(),
+                    semester.getSemesterNo(),
+                    semester.getYear(),
+                    semester.getStartDate(),
+                    semester.getEndDate());
+
+            return SemesterRequestResponseDTO.builder()
+                    .name(semester.getName())
+                    .year(semester.getYear())
+                    .semesterNo(semester.getSemesterNo())
+                    .startDate(semester.getStartDate())
+                    .endDate(semester.getEndDate())
+                    .build();
     }
 }
