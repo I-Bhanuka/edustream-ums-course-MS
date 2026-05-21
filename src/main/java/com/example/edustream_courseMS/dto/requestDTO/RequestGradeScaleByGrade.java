4113d65acaa2d8eb1480f61a8a465a96883f6e90
Isith Bhanuka <isithbhanuka2006@gmail.com>
2026-05-21 11:18:03 +0530

    feat: add RequestGradeScaleByGrade for GradeScale request

---
+++ src/main/java/com/example/edustream_courseMS/dto/requestDTO/RequestGradeScaleByGrade.java
@@ -0,0 +1,14 @@
+package com.example.edustream_courseMS.dto.requestDTO;
+
+import lombok.*;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+@Builder
+@ToString
+public class RequestGradeScaleByGrade {
+
+    private String grade;
+}
