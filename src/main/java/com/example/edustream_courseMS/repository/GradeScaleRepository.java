ddea92135cb1bf9e8b70d84c83d54d1df888d836
Isith Bhanuka <isithbhanuka2006@gmail.com>
2026-05-21 12:11:35 +0530

    feat: add findGradeAndGradePointByMark query method to retrieve grade and grade point by marks

--- src/main/java/com/example/edustream_courseMS/repository/GradeScaleRepository.java
+++ src/main/java/com/example/edustream_courseMS/repository/GradeScaleRepository.java
@@ -1,7 +1,9 @@
 package com.example.edustream_courseMS.repository;
 
+import com.example.edustream_courseMS.dto.responseDTO.GradeAndGradePointResponseDTO;
 import com.example.edustream_courseMS.entity.GradeScale;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
@@ -11,4 +13,12 @@ import java.util.UUID;
 public interface GradeScaleRepository extends JpaRepository<GradeScale, UUID> {
 
     Optional<GradeScale> findByGrade(String grade);
+
+    @Query("""
+    SELECT new com.example.edustream_courseMS.dto.responseDTO.GradeAndGradePointResponseDTO(
+        g.grade, g.gradePoint)
+    FROM GradeScale g
+    WHERE :mark >= g.min AND :mark <= g.max
+    """)
+    GradeAndGradePointResponseDTO findGradeAndGradePointByMark(double mark);
 }
