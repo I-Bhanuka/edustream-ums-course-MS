e051b3ebc5d2e74caf553c199e102d8ae04cfdbf
Isith Bhanuka <isithbhanuka2006@gmail.com>
2026-05-21 08:22:06 +0530

    feat: add getEnrollmentByIdService contract method into the interface

--- src/main/java/com/example/edustream_courseMS/service/StudentModuleService.java
+++ src/main/java/com/example/edustream_courseMS/service/StudentModuleService.java
@@ -1,9 +1,12 @@
 package com.example.edustream_courseMS.service;
 
 import com.example.edustream_courseMS.dto.requestDTO.EnrollStudentToModuleRequestDTO;
+import com.example.edustream_courseMS.dto.requestDTO.RequestModuleEnrollmentById;
 import com.example.edustream_courseMS.dto.responseDTO.EnrollStudentToModuleResponseDTO;
 
 public interface StudentModuleService {
 
     EnrollStudentToModuleResponseDTO enrollStudentInModuleService(EnrollStudentToModuleRequestDTO enrollStudentToModuleRequestDTO);
+
+    EnrollStudentToModuleResponseDTO getEnrollmentByIdService(RequestModuleEnrollmentById requestModuleEnrollmentById);
 }
