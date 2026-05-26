b20bca6a37099f70cf341edbf2ddc3369eab0be1
Isith Bhanuka <isithbhanuka2006@gmail.com>
2026-05-26 11:06:39 +0530

    refactor: refactor the GlobalExaceptionHandler to use the lib-common for the ErrorResponseDTO

--- src/main/java/com/example/edustream_courseMS/exception/GlobalExceptionHandler.java
+++ src/main/java/com/example/edustream_courseMS/exception/GlobalExceptionHandler.java
@@ -1,6 +1,6 @@
 package com.example.edustream_courseMS.exception;
 
-import com.example.edustream_courseMS.dto.responseDTO.ErrorResponseDTO;
+import com.example.edustream_lib_common.responseDTO.ErrorResponseDTO;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
 import org.springframework.http.converter.HttpMessageNotReadableException;
